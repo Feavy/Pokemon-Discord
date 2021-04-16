de9fa91e5994cdf04a8b672ed9ab78d36f1b5da0
Feavy <quentin071100@live.fr>
2021-04-16 18:55:46 +0200

    Added Tiled. Rewrote TMXMapReader to make it able to read files from classpath. Created a simple test that draw the map in a png file.

---
+++ src/main/resources/tilesets/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="pokemon" tilewidth="16" tileheight="16" tilecount="14758" columns="94">
+ <image source="./tileset.png" trans="ffffff" width="1504" height="2519"/>
+</tileset>
---
+++ src/test/java/fr/reminy/pokemon_discord/TiledTests.java
@@ -0,0 +1,34 @@
+package fr.reminy.pokemon_discord;
+
+
+import fr.reminy.pokemon_discord.tmx.ClasspathTMXMapReader;
+import org.junit.jupiter.api.Test;
+import org.mapeditor.core.Map;
+import org.mapeditor.core.MapLayer;
+import org.mapeditor.core.TileLayer;
+import org.mapeditor.view.OrthogonalRenderer;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.InputStream;
+
+public class TiledTests {
+
+    @Test
+    public void test1() throws Exception {
+        ClasspathTMXMapReader reader = new ClasspathTMXMapReader();
+        Map map = reader.readMap("/maps/map1.tmx");
+        MapLayer layer0 = map.getLayer(0);
+        System.out.println(layer0);
+        BufferedImage image = new BufferedImage(map.getWidth()*map.getTileWidth(), map.getHeight()*map.getTileHeight(), BufferedImage.TYPE_INT_RGB);
+        Graphics2D graphics = (Graphics2D) image.getGraphics();
+        graphics.setClip(0, 0, image.getWidth(), image.getHeight());
+        OrthogonalRenderer orthogonalRenderer = new OrthogonalRenderer(map);
+        orthogonalRenderer.paintTileLayer(graphics, (TileLayer) map.getLayer(0));
+        orthogonalRenderer.paintTileLayer(graphics, (TileLayer) map.getLayer(1));
+        ImageIO.write(image, "png", new File("output.png"));
+    }
+
+}
