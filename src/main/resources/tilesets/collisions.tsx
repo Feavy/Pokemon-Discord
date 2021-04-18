2882d9bc18ec5893f906479b730159d43e1e8d93
Feavy <quentin071100@live.fr>
2021-04-18 20:49:01 +0200

    Added collisions tileset.

---
+++ src/main/resources/tilesets/collisions.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="Collisions" tilewidth="16" tileheight="16" tilecount="6" columns="3">
+ <image source="collisions.png" width="48" height="32"/>
+</tileset>
